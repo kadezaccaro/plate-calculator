f9b2fa13c7decbc1a21afddaacdb992b47857ad3
kade <zaccaro.kade@gmail.com>
2023-01-07 17:32:04 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+// let plateBank = [
+//   [35, 35],
+//   [25, 25, 25, 25],
+//   [5, 5, 5, 5, 5, 5],
+//   [2.5, 2.5],
+//   [1, 1, 1, 1],
+// ];
+
+const form = document.querySelector(".form");
+const weightInput = document.getElementById("work-weight");
+const submitBtn = document.getElementById("submit-btn");
+const result = document.querySelector(".result");
+const barbells = document.getElementsByName("barbell");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const workWeight = weightInput.value;
+  let barWeight;
+  // check which bar is selected and assign bar weight that value
+  for (let i = 0; i < barbells.length; i++) {
+    if (barbells[i].checked) {
+      barWeight = barbells[i].value;
+    }
+  }
+  const weightPerSide = (workWeight - barWeight) / 2;
+  result.innerHTML = `<span class="lbs">${weightPerSide} lbs</span><br />on each side`;
+});
